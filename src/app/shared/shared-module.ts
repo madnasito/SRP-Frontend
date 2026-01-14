931f65707dd69ba1404d08966b174132bb3e56fc
MADNA <madnamv@gmail.com>
2026-01-14 12:16:30 -0400

    idk

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Header } from './header/header';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,8 @@ import { Header } from './header/header';
     Header
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     Header
